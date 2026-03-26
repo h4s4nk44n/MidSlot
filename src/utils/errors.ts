2a38ef0b8147d34f065fa306b8ef957b6f1a4d26
AKI <akerem.ince@gmail.com>
2026-03-26 22:42:43 +0200

    errors.middleware.ts & errors.ts

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -16,71 +16,71 @@
  last change: 26/03/26
  **/
 
-class AppError extends Error{
- statusCode: number;
- isOperational: boolean;
+export class AppError extends Error {
+  statusCode: number;
+  isOperational: boolean;
 
- constructor(message: string, statusCode: number, isOperational = true){
-  super(message);
-  this.statusCode = statusCode;
-  this.isOperational = isOperational;
-  this.name = this.constructor.name;
-  (Error as any).captureStackTrace(this, this.constructor);
- }
+  constructor(message: string, statusCode: number, isOperational = true) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+    this.name = this.constructor.name;
+    (Error as any).captureStackTrace(this, this.constructor);
+  }
 }
 
 /*  400 - Bad Request  */
-class BadRequestError extends AppError{
- constructor(message = "Bad Request") {
-  super(message, 400);
- }
+export class BadRequestError extends AppError {
+  constructor(message = "Bad Request") {
+    super(message, 400);
+  }
 }
 
 /*  401 - Unauthorised  */
-class UnauthorisedError extends AppError{
- constructor(message = "Unauthorised") {
-  super(message, 401);
- }
+export class UnauthorisedError extends AppError {
+  constructor(message = "Unauthorised") {
+    super(message, 401);
+  }
 }
 
 /*  403 - Forbidden  */
-class ForbiddenError extends AppError{
- constructor(message = "Forbidden") {
-  super(message, 403);
- }
+export class ForbiddenError extends AppError {
+  constructor(message = "Forbidden") {
+    super(message, 403);
+  }
 }
 
 /*  404 - Not Found  */
-class NotFoundError extends AppError{
- constructor(message = "Resource Not Found") {
-  super(message, 404);
- }
+export class NotFoundError extends AppError {
+  constructor(message = "Resource Not Found") {
+    super(message, 404);
+  }
 }
 
 /*  409 - Conflict  */
-class ConflictError extends AppError{
- constructor(message = "Conflict") {
-  super(message, 409);
- }
+export class ConflictError extends AppError {
+  constructor(message = "Conflict") {
+    super(message, 409);
+  }
 }
 
 /*  418 - I'm a Teapot  */
-class TeaPotError extends AppError{
- constructor(message = "I'm a teapot :)") {
-  super(message, 418);
- }
+export class TeaPotError extends AppError {
+  constructor(message = "I'm a teapot :)") {
+    super(message, 418);
+  }
 }
 
 /*  422 - Unprocessable Entity  */
-class UnprocessableEntityError extends AppError{
- constructor(message = "Unprocessable Entity") {
-  super(message, 422);
- }
+export class UnprocessableEntityError extends AppError {
+  constructor(message = "Unprocessable Entity") {
+    super(message, 422);
+  }
 }
 
 /*  429 - Too Many Requests  */
-class TooManyRequestsError extends AppError{
- constructor(message = "Too Many Requests") {
-  super(message, 429);
- }
+export class TooManyRequestsError extends AppError {
+  constructor(message = "Too Many Requests") {
+    super(message, 429);
+  }
 }
