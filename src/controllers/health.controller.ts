440ef262cbb0a24dd54c4d7ffe4c863807430580
Taha Turkay <tahaturkay01@gmail.com>
2026-03-26 13:55:24 +0200

    deneme

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from "express";
+import { Request, Response } from "express"; // deneme
 
 export const getHealthStatus = (_req: Request, res: Response): void => {
   res.status(200).json({ status: "ok" });
