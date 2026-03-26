51dba90e65fb099b29be106bca1522983aa1a6f6
AKI <akerem.ince@gmail.com>
2026-03-26 22:59:35 +0200

    index.ts 404 error status

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import dotenv from "dotenv";
 import healthRouter from "./routes/health.routes";
+import errorHandler from "./middlewares/error.middlewares";
 
 dotenv.config();
 
@@ -10,7 +11,19 @@ const PORT = parseInt(process.env.PORT || "3000", 10);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+/* <Routes> */
+
 app.use("/api", healthRouter);
+
+/* </Routes> */
+
+app.use((_req: Request, res: Response) => {
+  res.status(404).json({
+    error: "Route not found",
+    statusCode: 404,
+  });
+});
+app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`[MidSlot] Server running on http://localhost:${PORT}`);
