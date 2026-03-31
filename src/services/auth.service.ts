c1f40720c2ccd22e0a40542b85414fa65f0b3dcf
Taha Turkay <tahaturkay01@gmail.com>
2026-04-01 00:48:37 +0300

    Comments added.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -7,16 +7,16 @@ import { ConflictError, UnauthorisedError } from "../utils/errors";
 export const registerUser = async (data: RegisterInput) => {
   const { email, name, password, role } = data;
 
-  // 1. Email zaten kayıtlı mı?
+  // 1. Is email already registered?
   const existing = await prisma.user.findUnique({ where: { email } });
   if (existing) {
     throw new ConflictError("Email already registered");
   }
 
-  // 2. Şifreyi hashle
+  // 2. Hashing the password
   const hashedPassword = await bcrypt.hash(password, 10);
 
-  // 3. User oluştur (DOCTOR ise Doctor profili de)
+  // 3. Create user (If it is doctor; then also create doctor profile)
   const user = await prisma.user.create({
     data: {
       email,
@@ -44,10 +44,10 @@ export const registerUser = async (data: RegisterInput) => {
 export const loginUser = async (data: LoginInput) => {
   const { email, password } = data;
 
-  // 1. Kullanıcıyı bul
+  // 1. Find the user
   const user = await prisma.user.findUnique({ where: { email } });
 
-  // 2. Kullanıcı yoksa veya şifre yanlışsa — aynı hata mesajı (security)
+  // 2. If user doesn't exist or wrong password (same error message)
   if (!user) {
     throw new UnauthorisedError("Invalid email or password");
   }
@@ -57,14 +57,14 @@ export const loginUser = async (data: LoginInput) => {
     throw new UnauthorisedError("Invalid email or password");
   }
 
-  // 3. JWT oluştur
+  // 3. create JWT
   const token = jwt.sign(
   { userId: user.id, email: user.email, role: user.role },
   process.env.JWT_SECRET as string,
   { expiresIn: (process.env.JWT_EXPIRES_IN || "7d") as jwt.SignOptions["expiresIn"] }
 );
 
-  // 4. Password olmadan döndür
+  // 4. return without password
   const { password: _, ...userWithoutPassword } = user;
 
   return {
